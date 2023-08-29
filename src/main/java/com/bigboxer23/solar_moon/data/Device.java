1d1c70b13619baddcaf8065474ed7f75c207569d
Matt Jones <bigboxer23@gmail.com>
2023-08-29 14:49:45 -0500

    initial checkin

---
+++ src/main/java/com/bigboxer23/solar_moon/data/Device.java
@@ -0,0 +1,37 @@
+package com.bigboxer23.solar_moon.data;
+
+import lombok.Data;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbPartitionKey;
+
+/** */
+@Data
+@DynamoDbBean
+public class Device {
+	private String id;
+
+	private String name;
+
+	private String address;
+
+	private String site;
+
+	private String user;
+
+	private String password;
+
+	private String type;
+
+	private String deviceName;
+
+	private boolean subtraction;
+
+	@DynamoDbPartitionKey
+	public String getId() {
+		return id;
+	}
+
+	public boolean isPushedDevice() {
+		return getPassword() == null && getUser() == null;
+	}
+}
