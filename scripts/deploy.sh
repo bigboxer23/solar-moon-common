50f0696809a080f33c1ef42c3828b8d65e3ae9b2
Matt Jones <bigboxer23@gmail.com>
2023-10-11 21:18:26 -0500

    add version number to layer description (#79)

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,4 +9,4 @@ mkdir java/lib
 cp solar-moon-common-$version.jar java/lib
 zip -r solar-moon-common.zip java
 aws s3 cp solar-moon-common.zip s3://solarmoonanalytics/lambda/
-aws lambda publish-layer-version --layer-name solar-moon-common --compatible-architectures arm64 --compatible-runtimes java17 --region us-west-2 --content S3Bucket=solarmoonanalytics,S3Key=lambda/solar-moon-common.zip --output text
+aws lambda publish-layer-version --layer-name solar-moon-common --compatible-architectures arm64 --compatible-runtimes java17 --description $version --region us-west-2 --content S3Bucket=solarmoonanalytics,S3Key=lambda/solar-moon-common.zip --output text
