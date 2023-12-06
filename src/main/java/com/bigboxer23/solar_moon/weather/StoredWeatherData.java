c432208c45eac4c3f69fc9d3502ad957ba9a5c14
Matt Jones <bigboxer23@gmail.com>
2023-12-06 13:04:16 -0600

    feat: remove old, non updated weather information

--- src/main/java/com/bigboxer23/solar_moon/weather/StoredWeatherData.java
+++ src/main/java/com/bigboxer23/solar_moon/weather/StoredWeatherData.java
@@ -1,5 +1,6 @@
 package com.bigboxer23.solar_moon.weather;
 
+import com.bigboxer23.solar_moon.util.TimeConstants;
 import lombok.Data;
 import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean;
 import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbPartitionKey;
@@ -14,12 +15,15 @@ public class StoredWeatherData {
 
 	private long time;
 
+	private long expiration;
+
 	public StoredWeatherData() {}
 
 	public StoredWeatherData(double latitude, double longitude, String weather, long time) {
 		setLatitudeLongitude(latitude + "," + longitude);
 		setWeather(weather);
 		setTime(time);
+		setExpiration((System.currentTimeMillis() + TimeConstants.THIRTY_DAYS) / 1000);
 	}
 
 	@DynamoDbPartitionKey
