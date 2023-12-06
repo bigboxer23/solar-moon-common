31ca5c0b47fe6e952c96a71038e15e3999773ed2
Matt Jones <bigboxer23@gmail.com>
2023-12-06 09:41:52 -0600

    feat: add hook to send emails to real recipient as well as admin (#181)

--- src/main/java/com/bigboxer23/solar_moon/notifications/NotificationComponent.java
+++ src/main/java/com/bigboxer23/solar_moon/notifications/NotificationComponent.java
@@ -5,6 +5,8 @@ import com.github.mustachejava.DefaultMustacheFactory;
 import com.github.mustachejava.MustacheFactory;
 import java.io.IOException;
 import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
@@ -24,6 +26,8 @@ public class NotificationComponent {
 
 	private final String recipientOverride = PropertyUtils.getProperty("recipient_override");
 
+	private final String additionalRecipient = PropertyUtils.getProperty("additional_recipient");
+
 	public void sendNotification(String recipient, String subject, EmailTemplateContent template) {
 		StringWriter content = new StringWriter();
 		try {
@@ -32,28 +36,37 @@ public class NotificationComponent {
 			logger.warn("sendNotification: template error", e);
 			return;
 		}
-		try (SesClient client = SesClient.builder()
-				.region(Region.US_WEST_2)
-				.credentialsProvider(DefaultCredentialsProvider.create())
-				.build(); ) {
-			logger.info("Sending email to " + recipient);
-			client.sendEmail(SendEmailRequest.builder()
-					.destination(Destination.builder()
-							.toAddresses(getRecipient(recipient))
-							.build())
-					.message(Message.builder()
-							.subject(Content.builder().data(subject).build())
-							.body(Body.builder()
-									.html(Content.builder()
-											.data(content.toString())
-											.build())
-									.build())
-							.build())
-					.source(SENDER)
-					.build());
-		} catch (SesException e) {
-			logger.warn(e.awsErrorDetails().errorMessage(), e);
+		getRecipients(recipient).forEach(r -> {
+			try (SesClient client = SesClient.builder()
+					.region(Region.US_WEST_2)
+					.credentialsProvider(DefaultCredentialsProvider.create())
+					.build(); ) {
+				logger.info("Sending email to " + recipient);
+				client.sendEmail(SendEmailRequest.builder()
+						.destination(Destination.builder().toAddresses(r).build())
+						.message(Message.builder()
+								.subject(Content.builder().data(subject).build())
+								.body(Body.builder()
+										.html(Content.builder()
+												.data(content.toString())
+												.build())
+										.build())
+								.build())
+						.source(SENDER)
+						.build());
+			} catch (SesException e) {
+				logger.warn(e.awsErrorDetails().errorMessage(), e);
+			}
+		});
+	}
+
+	private List<String> getRecipients(String recipient) {
+		List<String> recipients = new ArrayList<>();
+		recipients.add(getRecipient(recipient));
+		if (!StringUtils.isBlank(additionalRecipient)) {
+			recipients.add(additionalRecipient);
 		}
+		return recipients;
 	}
 
 	private String getRecipient(String recipient) {
