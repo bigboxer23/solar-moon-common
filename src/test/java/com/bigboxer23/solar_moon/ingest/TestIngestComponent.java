f9728069517c9877a0f449be9cfdd89dafd876df
Matt Jones <bigboxer23@gmail.com>
2024-03-13 11:24:34 -0500

    fix: site creation for SMA device doesn't stamp site properly

--- src/test/java/com/bigboxer23/solar_moon/ingest/TestIngestComponent.java
+++ src/test/java/com/bigboxer23/solar_moon/ingest/TestIngestComponent.java
@@ -9,17 +9,29 @@ import org.junit.jupiter.api.Test;
 public class TestIngestComponent implements IComponentRegistry, MeterConstants {
 	@Test
 	public void maybeCorrectForRollover() {
-		assertEquals(OBVIOUS_ROLLOVER_MARGIN, generationComponent.maybeCorrectForRollover(10000, OBVIOUS_ROLLOVER_MARGIN));
+		assertEquals(
+				OBVIOUS_ROLLOVER_MARGIN, generationComponent.maybeCorrectForRollover(10000, OBVIOUS_ROLLOVER_MARGIN));
 		assertEquals(OBVIOUS_ROLLOVER_MARGIN, generationComponent.maybeCorrectForRollover(0, OBVIOUS_ROLLOVER_MARGIN));
-		assertEquals(OBVIOUS_ROLLOVER_MARGIN, generationComponent.maybeCorrectForRollover(999, OBVIOUS_ROLLOVER_MARGIN));
-		assertEquals(OBVIOUS_ROLLOVER_MARGIN, generationComponent.maybeCorrectForRollover(999, OBVIOUS_ROLLOVER_MARGIN));
+		assertEquals(
+				OBVIOUS_ROLLOVER_MARGIN, generationComponent.maybeCorrectForRollover(999, OBVIOUS_ROLLOVER_MARGIN));
+		assertEquals(
+				OBVIOUS_ROLLOVER_MARGIN, generationComponent.maybeCorrectForRollover(999, OBVIOUS_ROLLOVER_MARGIN));
 		assertEquals(
 				OBVIOUS_ROLLOVER + OBVIOUS_ROLLOVER_MARGIN,
-				generationComponent.maybeCorrectForRollover(OBVIOUS_ROLLOVER, OBVIOUS_ROLLOVER + OBVIOUS_ROLLOVER_MARGIN));
-		assertEquals(OBVIOUS_ROLLOVER_MARGIN, generationComponent.maybeCorrectForRollover(OBVIOUS_ROLLOVER - 1, OBVIOUS_ROLLOVER_MARGIN));
-		assertEquals(OBVIOUS_ROLLOVER + OBVIOUS_ROLLOVER_MARGIN - 1, generationComponent.maybeCorrectForRollover(OBVIOUS_ROLLOVER - 1, OBVIOUS_ROLLOVER_MARGIN - 1));
-		assertEquals(OBVIOUS_ROLLOVER + 10, generationComponent.maybeCorrectForRollover(OBVIOUS_ROLLOVER - (OBVIOUS_ROLLOVER_MARGIN - 1), 10));
-		assertEquals(OBVIOUS_ROLLOVER + 10, generationComponent.maybeCorrectForRollover(OBVIOUS_ROLLOVER - (OBVIOUS_ROLLOVER_MARGIN - 1), 10));
+				generationComponent.maybeCorrectForRollover(
+						OBVIOUS_ROLLOVER, OBVIOUS_ROLLOVER + OBVIOUS_ROLLOVER_MARGIN));
+		assertEquals(
+				OBVIOUS_ROLLOVER_MARGIN,
+				generationComponent.maybeCorrectForRollover(OBVIOUS_ROLLOVER - 1, OBVIOUS_ROLLOVER_MARGIN));
+		assertEquals(
+				OBVIOUS_ROLLOVER + OBVIOUS_ROLLOVER_MARGIN - 1,
+				generationComponent.maybeCorrectForRollover(OBVIOUS_ROLLOVER - 1, OBVIOUS_ROLLOVER_MARGIN - 1));
+		assertEquals(
+				OBVIOUS_ROLLOVER + 10,
+				generationComponent.maybeCorrectForRollover(OBVIOUS_ROLLOVER - (OBVIOUS_ROLLOVER_MARGIN - 1), 10));
+		assertEquals(
+				OBVIOUS_ROLLOVER + 10,
+				generationComponent.maybeCorrectForRollover(OBVIOUS_ROLLOVER - (OBVIOUS_ROLLOVER_MARGIN - 1), 10));
 		assertEquals(10, generationComponent.maybeCorrectForRollover(OBVIOUS_ROLLOVER - OBVIOUS_ROLLOVER_MARGIN, 10));
 	}
 }
