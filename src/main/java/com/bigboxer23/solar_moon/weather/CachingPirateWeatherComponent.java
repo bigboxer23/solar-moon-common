205e4bebf6e23c2343b113994ac0997a13f96dd0
Matt Jones <bigboxer23@gmail.com>
2025-03-01 10:03:10 -0600

    feat: removing guava, moving to Caffeine

--- src/main/java/com/bigboxer23/solar_moon/weather/CachingPirateWeatherComponent.java
+++ src/main/java/com/bigboxer23/solar_moon/weather/CachingPirateWeatherComponent.java
@@ -1,42 +1,34 @@
 package com.bigboxer23.solar_moon.weather;
 
-import com.google.common.cache.CacheBuilder;
-import com.google.common.cache.CacheLoader;
-import com.google.common.cache.LoadingCache;
+import com.github.benmanes.caffeine.cache.Caffeine;
+import com.github.benmanes.caffeine.cache.LoadingCache;
+import java.time.Duration;
 import java.util.Optional;
-import java.util.concurrent.TimeUnit;
 
 /** */
 public class CachingPirateWeatherComponent extends PirateWeatherComponent {
-	private final LoadingCache<String, Optional<PirateWeatherData>> weatherCache = CacheBuilder.newBuilder()
+	private final LoadingCache<String, Optional<PirateWeatherData>> weatherCache = Caffeine.newBuilder()
 			.maximumSize(10)
-			.expireAfterWrite(10, TimeUnit.SECONDS)
-			.build(new CacheLoader<>() {
-				public Optional<PirateWeatherData> load(String key) {
-					return CachingPirateWeatherComponent.super.getWeather(
-							Double.parseDouble(key.substring(0, key.indexOf(":"))),
-							Double.parseDouble(key.substring(key.indexOf(":") + 1)));
-				}
-			});
+			.expireAfterWrite(Duration.ofSeconds(10))
+			.build(key -> CachingPirateWeatherComponent.super.getWeather(
+					Double.parseDouble(key.substring(0, key.indexOf(":"))),
+					Double.parseDouble(key.substring(key.indexOf(":") + 1))));
 
-	private final LoadingCache<String, Long> lastUpdateCache = CacheBuilder.newBuilder()
+	private final LoadingCache<String, Long> lastUpdateCache = Caffeine.newBuilder()
 			.maximumSize(10)
-			.expireAfterWrite(10, TimeUnit.SECONDS)
-			.build(new CacheLoader<>() {
-				public Long load(String key) {
-					return CachingPirateWeatherComponent.super.getLastUpdate(
-							Double.parseDouble(key.substring(0, key.indexOf(":"))),
-							Double.parseDouble(key.substring(key.indexOf(":") + 1)));
-				}
-			});
+			.expireAfterWrite(Duration.ofSeconds(10))
+			.build(key -> CachingPirateWeatherComponent.super.getLastUpdate(
+					Double.parseDouble(key.substring(0, key.indexOf(":"))),
+					Double.parseDouble(key.substring(key.indexOf(":") + 1))));
 
 	@Override
 	public long getLastUpdate(double latitude, double longitude) {
-		return lastUpdateCache.getUnchecked(latitude + ":" + longitude);
+		Long timestamp = lastUpdateCache.getIfPresent(latitude + ":" + longitude);
+		return timestamp != null ? timestamp : 0L;
 	}
 
 	@Override
 	public Optional<PirateWeatherData> getWeather(double latitude, double longitude) {
-		return weatherCache.getUnchecked(latitude + ":" + longitude);
+		return weatherCache.get(latitude + ":" + longitude);
 	}
 }
