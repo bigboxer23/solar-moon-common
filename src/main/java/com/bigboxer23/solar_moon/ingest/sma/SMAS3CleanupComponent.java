2ce803cfc958efc9d0dda6a0909fdf46340628c6
Matt Jones <bigboxer23@gmail.com>
2025-10-12 09:48:55 -0500

    feat: adding lombok version so it's not defined in two places (#757)

--- src/main/java/com/bigboxer23/solar_moon/ingest/sma/SMAS3CleanupComponent.java
+++ src/main/java/com/bigboxer23/solar_moon/ingest/sma/SMAS3CleanupComponent.java
@@ -2,26 +2,27 @@ package com.bigboxer23.solar_moon.ingest.sma;
 
 import com.bigboxer23.solar_moon.IComponentRegistry;
 import com.bigboxer23.utils.properties.PropertyUtils;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
-import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
-import software.amazon.awssdk.services.s3.model.ListObjectsV2Request;
-import software.amazon.awssdk.services.s3.model.ListObjectsV2Response;
-import software.amazon.awssdk.services.s3.model.S3Object;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.*;
 
 /** Contains logic to look through S3 bucket and cleanup/remove any empty directory structure */
 @Slf4j
 public class SMAS3CleanupComponent {
 	private final String bucket = PropertyUtils.getProperty("ftp.s3.bucket");
 
-	// TODO: look at logic to deal w/stale data
+	protected S3Client getS3Client() {
+		return IComponentRegistry.smaIngestComponent.getS3Client();
+	}
 
 	public void cleanupEmptyFTPS3Folders() {
 
 		int count = 0;
 		String continuationToken = null;
 		do {
-			ListObjectsV2Response response = IComponentRegistry.smaIngestComponent
-					.getS3Client()
+			ListObjectsV2Response response = getS3Client()
 					.listObjectsV2(ListObjectsV2Request.builder()
 							.bucket(bucket)
 							.continuationToken(continuationToken)
@@ -30,8 +31,7 @@ public class SMAS3CleanupComponent {
 				log.info(count + " checking " + object.key());
 				if (isEmptyFolder(object.key())) {
 					log.info(count + " deleting " + object.key());
-					IComponentRegistry.smaIngestComponent
-							.getS3Client()
+					getS3Client()
 							.deleteObject(DeleteObjectRequest.builder()
 									.bucket(bucket)
 									.key(object.key())
@@ -51,8 +51,7 @@ public class SMAS3CleanupComponent {
 			return false; // Root folder
 		}
 		// Check if more than 1 object is found
-		for (S3Object s3Object : IComponentRegistry.smaIngestComponent
-				.getS3Client()
+		for (S3Object s3Object : getS3Client()
 				.listObjectsV2(ListObjectsV2Request.builder()
 						.bucket(bucket)
 						.prefix(folderKey)
@@ -65,5 +64,60 @@ public class SMAS3CleanupComponent {
 		}
 		// Only the folder key itself is found, hence it's empty
 		return true;
+	}
+
+	/**
+	 * Deletes a non-empty S3 folder and all its contents.
+	 *
+	 * @param folderPrefix the folder prefix to delete (will automatically append "/" if missing)
+	 */
+	public void deleteS3Folder(String folderPrefix) {
+		if (!folderPrefix.endsWith("/")) {
+			folderPrefix += "/";
+		}
+
+		log.info("Deleting S3 folder: {}", folderPrefix);
+
+		ListObjectsV2Request listRequest = ListObjectsV2Request.builder()
+				.bucket(bucket)
+				.prefix(folderPrefix)
+				.build();
+
+		ListObjectsV2Response listResponse;
+		int totalDeleted = 0;
+
+		do {
+			listResponse = getS3Client().listObjectsV2(listRequest);
+
+			if (!listResponse.contents().isEmpty()) {
+				List<ObjectIdentifier> keys = listResponse.contents().stream()
+						.map(s3Object ->
+								ObjectIdentifier.builder().key(s3Object.key()).build())
+						.collect(Collectors.toList());
+
+				DeleteObjectsRequest deleteRequest = DeleteObjectsRequest.builder()
+						.bucket(bucket)
+						.delete(Delete.builder().objects(keys).build())
+						.build();
+
+				DeleteObjectsResponse deleteResponse = getS3Client().deleteObjects(deleteRequest);
+
+				totalDeleted += deleteResponse.deleted().size();
+				log.info("Deleted {} objects from {}", deleteResponse.deleted().size(), folderPrefix);
+
+				if (!deleteResponse.errors().isEmpty()) {
+					deleteResponse
+							.errors()
+							.forEach(error -> log.error("Failed to delete {}: {}", error.key(), error.message()));
+				}
+			}
+
+			listRequest = listRequest.toBuilder()
+					.continuationToken(listResponse.nextContinuationToken())
+					.build();
+
+		} while (listResponse.isTruncated());
+
+		log.info("Completed deletion of folder {}. Total objects deleted: {}", folderPrefix, totalDeleted);
 	}
 }
