132e9c64ec72dd1d3fdf09b5dd8435cc69816aea
Matt Jones <bigboxer23@gmail.com>
2025-11-09 20:37:02 -0600

    feat: refactoring trial data to return in a more consistent state.

--- src/main/java/com/bigboxer23/solar_moon/aggregated/overview/OverviewData.java
+++ src/main/java/com/bigboxer23/solar_moon/aggregated/overview/OverviewData.java
@@ -2,14 +2,16 @@ package com.bigboxer23.solar_moon.aggregated.overview;
 
 import com.bigboxer23.solar_moon.data.Alarm;
 import com.bigboxer23.solar_moon.data.Device;
-import com.bigboxer23.solar_moon.subscription.IHasSubscriptionDate;
+import com.bigboxer23.solar_moon.data.Subscription;
+import com.bigboxer23.solar_moon.subscription.IHasSubscription;
 import java.util.List;
 import java.util.Map;
 import lombok.Data;
 
 /** */
 @Data
-public class OverviewData implements IHasSubscriptionDate {
+public class OverviewData implements IHasSubscription
+{
 	public OverviewData(List<Device> devices, List<Alarm> alarms) {
 		setDevices(devices);
 		setAlarms(alarms);
@@ -19,7 +21,7 @@ public class OverviewData implements IHasSubscriptionDate {
 
 	private List<Alarm> alarms;
 
-	private long trialDate = -1;
+	private Subscription subscription;
 
 	private OverviewSiteData overall;
 
