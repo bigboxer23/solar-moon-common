139b2b1fe161a85861ee3fd08e3ac4c1cfde4461
Matt Jones <bigboxer23@gmail.com>
2025-12-02 18:33:48 -0600

    feat: adding test parallelism (#803)

--- src/test/java/com/bigboxer23/solar_moon/ingest/IngestComponentTest.java
+++ src/test/java/com/bigboxer23/solar_moon/ingest/IngestComponentTest.java
@@ -23,9 +23,12 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
+@Execution(ExecutionMode.SAME_THREAD)
 class IngestComponentTest {
 
 	private IngestComponent component;
